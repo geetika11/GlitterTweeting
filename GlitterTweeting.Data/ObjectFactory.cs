2db81a531af9245e80cee01b099d16d77af82378
geetika11 <geetika.varshney@nagarro.com>
2018-12-20 17:28:47 +0530

    Register

---
+++ GlitterTweeting.Data/ObjectFactory.cs
@@ -0,0 +1,25 @@
+﻿using GlitterTweeting.Shared.DTO;
+using System;
+
+namespace GlitterTweeting.Data
+{
+    class ObjectFactory
+    {
+        public static User CreateNewUserObject(UserRegisterDTO user)
+        {
+            return new User
+            {
+
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email,
+                PhoneNumber = user.PhoneNumber,
+                Image = user.Image,
+                Country = user.Country,
+                PasswordHash = user.Password,
+                ID = Guid.NewGuid()
+            };
+        }
+    }
+
+}
